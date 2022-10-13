518df66d39afce00cffc4dda8f3cf78f051959fd
FOR <forypipi@163.com>
2022-10-13 22:31:10 +0800

    Initial commit

--- bloom filter/setup.py
+++ bloom filter/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='BloomFilter',
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.2',
     install_requires=[          # 添加了依赖的 package
         'mmh3==3.0.0',
         'math',
